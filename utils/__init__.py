3cdc3f6fde76bcf2f6712470ffc3f1cb8e919f07
hagai.tregerman <hagai1973@gmail.com>
2026-02-13 23:43:12 +0200

    Remove unused screenshot_helper.py and fix utils __init__.py imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,13 +3,9 @@ Utilities package
 """
 
 from .logger import Logger
-from .screenshot_helper import ScreenshotHelper
 from .config_reader import ConfigReader
-from .locator_helper import LocatorHelper
 
 __all__ = [
     'Logger',
-    'ScreenshotHelper',
-    'ConfigReader',
-    'LocatorHelper'
+    'ConfigReader'
 ]
